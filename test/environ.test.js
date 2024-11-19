abbaab364e693cd1413d547b611b431aa34a9383
Colin-Stark <collinscodes@gmail.com>
2024-11-19 16:42:15 -0500

    Hooks working internally

--- test/environ.test.js
+++ test/environ.test.js
@@ -7,6 +7,6 @@ describe('Check that the environmental variable can be read', () => {
     test('Check the data type and value of the port', () => {
         // Test both data type and value of the port in a single test
         expect(typeof port).toBe('string');
-        expect(port).toBe('80003');
+        expect(port).toBe('8000');
     });
 });
