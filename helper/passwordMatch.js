a1000a9a39b3c071b415326f160d4c72fd90bbc8
Inu(フェニックス) <dasujoy1999@gmail.com>
2024-11-24 13:36:40 +0530

    password matching changes

--- helper/passwordMatch.js
+++ helper/passwordMatch.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 
 /**
  * Hash the password if both passwords match.
@@ -7,15 +8,30 @@ const bcrypt = require('bcryptjs');
  * @returns {string|Error} - Hashed password or error
  */
 function hashPassword(password, confirmPassword) {
-    if (password !== confirmPassword) {
-        throw new Error('Passwords do not match');
-    }
+  /**
+   * Using crypto library to hash the passowrd for comparison
+   */
+  const hashedPassword = crypto
+    .createHash("sha512")
+    .update(password)
+    .digest("hex");
+  const hashedConfirmPassword = crypto
+    .createHash("sha512")
+    .update(confirmPassword)
+    .digest("hex");
 
-    if (password.length < 8 || password.length > 20) {
-        throw new Error('Password must be between 8 and 20 characters');
-    }
+  // comparing both hashesh
+  const bufferA = Buffer.from(hashedPassword);
+  const bufferB = Buffer.from(hashedConfirmPassword);
 
-    return bcrypt.hashSync(password, 12);  // Synchronous hash
+  if (!crypto.timingSafeEqual(bufferA, bufferB))
+    throw new Error("Passwords do not match");
+
+  if (password.length < 8 || password.length > 20) {
+    throw new Error("Password must be between 8 and 20 characters");
+  }
+
+  return bcrypt.hashSync(password, 12); // Synchronous hash
 }
 
 module.exports = { hashPassword };
