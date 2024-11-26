cffac8b51dbc0dfe960494fd641c77e6235d2d7c
akakadiya1 <akakadiya1@myseneca.ca>
2024-11-26 14:00:13 -0500

    Add PasswordComplexity Functionality

--- helper/passwordMatch.js
+++ helper/passwordMatch.js
@@ -11,9 +11,16 @@ const hashPassword = async (password, confirmPassword) => {
     throw new Error('Passwords do not match');
   }
 
-  const passwordRegex = /^.{8,20}$/;
-  if (!passwordRegex.test(password)) {
+  // Validate password length
+  const passwordLengthRegex = /^.{8,20}$/;
+  if (!passwordLengthRegex.test(password)) {
     throw new Error('Password must be between 8 and 20 characters');
+  }
+
+  // Validate password complexity
+  const passwordComplexityRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).+$/;
+  if (!passwordComplexityRegex.test(password)) {
+    throw new Error('Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character');
   }
 
   const newPassword = await bcrypt.hash(password, parseInt(process.env.SALTROUNDS));
--- test/passwordMatch.test.js
+++ test/passwordMatch.test.js
@@ -10,8 +10,8 @@ describe("hashPassword function", () => {
    * @returns {void}
    */
   test("should hash the password when passwords match", async () => {
-    const password = "mySecurePassword";
-    const confirmPassword = "mySecurePassword";
+    const password = "mySecurePassword@1";
+    const confirmPassword = "mySecurePassword@1";
 
     const hashedPassword = await hashPassword(password, confirmPassword);
 
@@ -32,32 +32,26 @@ describe("hashPassword function", () => {
 
     // Expect error to be thrown due to password mismatch
 
-    expect(async () => {
-      await hashPassword(password, confirmPassword);
-    }).rejects.toThrow("Passwords do not match");
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Passwords do not match");
   });
 
   /**
    * Test case to check if the function throws an error when passwords are empty.
    * @returns {void}
    */
-  test("should throw an error if passwords are empty", () => {
+  test("should throw an error if passwords are empty", async () => {
     const password = "";
     const confirmPassword = "";
 
     // Expect error to be thrown due to password mismatch (both are empty)
-    expect(async () => {
-      await hashPassword(password, confirmPassword);
-    }).rejects.toThrow("Password must be between 8 and 20 characters");
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Password must be between 8 and 20 characters");
   });
 
-  test("shpuld throw an error if password length of short than 8", async () => {
+  test("should throw an error if password length of short than 8", async () => {
     const password = "hi";
     const confirmPassword = "hi";
 
-    expect(async () => {
-      await hashPassword(password, confirmPassword);
-    }).rejects.toThrow("Password must be between 8 and 20 characters");
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Password must be between 8 and 20 characters");
   });
 
   /**
@@ -69,8 +63,50 @@ describe("hashPassword function", () => {
     const confirmPassword = "longerPassword";
 
     // Expect error to be thrown due to password mismatch (different lengths)
-    expect(async () => {
-      await hashPassword(password, confirmPassword);
-    }).rejects.toThrow("Passwords do not match");
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Passwords do not match");
+  });
+
+  /**
+   * Test case to check if the function throws an error when password doesn't contain uppercase letter.
+   * @returns {void}
+   */
+  test("should throw an error if password does not contain an uppercase letter", async () => {
+    const password = "lowercase1@";
+    const confirmPassword = "lowercase1@";
+
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character");
+  });
+
+  /**
+   * Test case to check if the function throws an error when password doesn't contain lowercase letter.
+   * @returns {void}
+   */
+  test("should throw an error if password does not contain a lowercase letter", async () => {
+    const password = "UPPERCASE1@";
+    const confirmPassword = "UPPERCASE1@";
+
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character");
+  });
+
+  /**
+   * Test case to check if the function throws an error when password doesn't have digit.
+   * @returns {void}
+   */
+  test("should throw an error if password does not contain a digit", async () => {
+    const password = "NoDigit@";
+    const confirmPassword = "NoDigit@";
+
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character");
+  });
+
+  /**
+   * Test case to check if the function throws an error when password doesn't contain a special character.
+   * @returns {void}
+   */
+  test("should throw an error if password does not contain a special character", async () => {
+    const password = "NoSpecial1";
+    const confirmPassword = "NoSpecial1";
+
+    await expect(hashPassword(password, confirmPassword)).rejects.toThrow("Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character");
   });
 });
