c37a249b371be46e97cd37b93152cc96eea3d528
Colin-Stark <collinscodes@gmail.com>
2024-11-30 23:29:51 -0500

    I forgot to add it to the github workflow

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
 
 const connectDB = async () => {
     try {
-        console.log(`Connecting to MongoDB at ${process.env.MONGO_URI} with a port number of ${process.env.PORT}`);
+        console.log(`Connecting to MongoDB at ${process.env.mango_uri} with a port number of ${process.env.PORT}`);
         // await mongoose.connect(process.env.mongo_uri);
         console.log('Connected to MongoDB');
     } catch (error) {
