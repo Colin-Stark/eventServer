4443c1c7bd12a09c27fdb348bbe0fff6665052b5
Colin-Stark <collinscodes@gmail.com>
2024-11-30 23:31:04 -0500

    test for pushing to production

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ app.get('/', (req, res) => {
 
 const connectDB = async () => {
     try {
-        console.log(`Connecting to MongoDB at ${process.env.mango_uri} with a port number of ${process.env.PORT}`);
-        // await mongoose.connect(process.env.mongo_uri);
+        await mongoose.connect(process.env.mongo_uri);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('MongoDB Connection Error:', error);
