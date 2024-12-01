df76a99d0e9288079346a94adb7b0245c6947ca6
Colin-Stark <collinscodes@gmail.com>
2024-11-30 23:16:33 -0500

    checking why uri is undefined

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ app.get('/', (req, res) => {
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.mongo_uri);
+        console.log(process.env.mongo_uri);
+        // await mongoose.connect(process.env.mongo_uri);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('MongoDB Connection Error:', error);
